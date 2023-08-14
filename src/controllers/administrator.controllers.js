e66e12d60c8a7d151558e89e6199d467bc16188d
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-08-14 12:00:09 -0500

    Apy

--- src/controllers/administrator.controllers.js
+++ src/controllers/administrator.controllers.js
@@ -22,7 +22,12 @@ const create = catchError(async(req, res) => {
 
 const getOne = catchError(async(req, res) => {
     const { id } = req.params;
-    const result = await Administrator.findByPk(id);
+    const result = await Administrator.findByPk(id, {
+        include:[{
+            model: User,
+            include: Purchase
+        }]
+    });
     if(!result) return res.sendStatus(404);
     return res.json(result);
 });

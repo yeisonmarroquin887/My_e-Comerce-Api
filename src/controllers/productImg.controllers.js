2ff2b9e7c3180414b4086f0182f9f5fe3f8aa7df
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-09-11 16:32:37 -0500

    my apy

--- src/controllers/productImg.controllers.js
+++ src/controllers/productImg.controllers.js
@@ -2,31 +2,38 @@ const catchError = require('../utils/catchError');
 const { uploadToCloudinary, deleteFromCloudinary } = require('../utils/cloudinary');
 const ProductImg = require('../models/ProductImg');
 
-
-const getAll = catchError(async(req, res) => {
-    const result = await ProductImg.findAll()
-    return res.json(result)
+const getAll = catchError(async (req, res) => {
+  const result = await ProductImg.findAll();
+  return res.json(result);
 });
 
-const create = catchError(async(req, res) => {
-    const { path, filename } = req.file;
+const create = catchError(async (req, res) => {
+  const images = req.files; // Obtén todas las imágenes enviadas en el formulario
+  const uploadedImages = [];
+
+  // Itera sobre las imágenes y súbela a Cloudinary
+  for (const image of images) {
+    const { path, filename } = image;
     const { url, public_id } = await uploadToCloudinary(path, filename);
-    const body = { url, filename: public_id }
-    const image = await ProductImg.create(body);
-    return res.status(201).json(image);
+    const body = { url, filename: public_id };
+    const createdImage = await ProductImg.create(body);
+    uploadedImages.push(createdImage);
+  }
+
+  return res.status(201).json(uploadedImages);
 });
 
-const remove = catchError(async(req, res) => {
-    const { id } = req.params;
-    const image = await ProductImg.findByPk(id);
-    if(!image) return res.sendStatus(404);
-    await deleteFromCloudinary(image.filename);
-    await image.destroy();
-    return res.sendStatus(204);
+const remove = catchError(async (req, res) => {
+  const { id } = req.params;
+  const image = await ProductImg.findByPk(id);
+  if (!image) return res.sendStatus(404);
+  await deleteFromCloudinary(image.filename);
+  await image.destroy();
+  return res.sendStatus(204);
 });
 
 module.exports = {
-    getAll,
-    create, 
-    remove
-}
+  getAll,
+  create,
+  remove
+};

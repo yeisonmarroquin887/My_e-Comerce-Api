c15bf01ad1bb109156279b4cbeca319a06f50e1a
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-09-14 20:24:19 -0500

    My Apy

--- src/controllers/productImg.controllers.js
+++ src/controllers/productImg.controllers.js
@@ -3,50 +3,67 @@ const { uploadToCloudinary, deleteFromCloudinary } = require('../utils/cloudinar
 const ProductImg = require('../models/ProductImg');
 const Product = require('../models/Product');
 const multer = require('multer');
+const path = require('path'); // Agregamos la librería path
+const fs = require('fs');
+
 
 const upload = multer({ dest: 'temp/' });
 
 const getAll = catchError(async (req, res) => {
-  const img = await ProductImg.findAll()
-  return res.json(img)
+  const img = await ProductImg.findAll();
+  return res.json(img);
 });
 
 const create = catchError(async (req, res) => {
-    const images = req.files;
-    const uploadedImages = [];
+  const images = req.files;
+  const uploadedImages = [];
+  const { productId } = req.params;
+  const product = await Product.findByPk(productId);
+
+  if (!product) {
+    return res.status(404).json({ message: 'Producto no encontrado' });
+  }
+
+  for (const imageFile of images) {
+    const { path, filename } = imageFile;
     
-    const { productId } = req.params;
-
-    const product = await Product.findByPk(productId);
-
-    if (!product) {
-      return res.status(404).json({ message: 'Producto no encontrado' });
-    }
-
-    for (const imageFile of images) {
-      const { path, filename } = imageFile;
+    // Verificamos que el archivo local exista
+    if (fs.existsSync(path)) {
       const { url, public_id } = await uploadToCloudinary(path, filename);
       const body = { url, filename: public_id };
       
       const image = await ProductImg.create(body);
       await image.setProduct(product);
       uploadedImages.push(image);
+    } else {
+      console.error(`Archivo no encontrado en la ruta: ${path}`);
     }
+  }
 
-    return res.status(201).json(uploadedImages);
+  return res.status(201).json(uploadedImages);
 });
 
 const remove = catchError(async (req, res) => {
-    const { id } = req.params;
-    const image = await ProductImg.findByPk(id);
-    if (!image) return res.sendStatus(404);
-    await deleteFromCloudinary(image.filename);
-    await image.destroy();
-    return res.sendStatus(204);
+  const { id } = req.params;
+  const image = await ProductImg.findByPk(id);
+
+  if (!image) return res.sendStatus(404);
+
+  // Eliminamos de Cloudinary
+  await deleteFromCloudinary(image.filename);
+
+  // Eliminamos el archivo local (si es necesario)
+  const localFilePath = path.join('temp', image.filename);
+  if (fs.existsSync(localFilePath)) {
+    fs.unlinkSync(localFilePath);
+  }
+
+  await image.destroy();
+  return res.sendStatus(204);
 });
 
 module.exports = {
-    getAll,
-    create,
-    remove
+  getAll,
+  create,
+  remove
 };

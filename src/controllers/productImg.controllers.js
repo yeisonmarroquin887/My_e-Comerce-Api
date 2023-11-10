01b500fd95fa51b20fb33f969870ea25544718a2
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-11-10 16:50:48 -0500

    My Apy

--- src/controllers/productImg.controllers.js
+++ src/controllers/productImg.controllers.js
@@ -1,51 +1,29 @@
 const catchError = require('../utils/catchError');
-const { deleteFromCloudinary } = require('../utils/cloudinary');
+const { deleteFromCloudinary, uploadToCloudinary } = require('../utils/cloudinary');
 const ProductImg = require('../models/ProductImg');
 const path = require('path');
 const fs = require('fs');
-const { uploadToCloudinary } = require('../utils/cloudinary');
 
 
 const getAll = catchError(async (req, res) => {
   const img = await ProductImg.findAll();
   return res.json(img);
 });
-const create = catchError(async (req, res) => {
-  const { files } = req;
 
-  if (!files || files.length !== 3) {
-      return res.status(400).json({ message: "Se requieren exactamente 3 imágenes" });
-  }
-
-  const imageArray = [];
-
-  for (const file of files) {
-      const { path, filename } = file;
-      const { url, public_id } = await uploadToCloudinary(path, filename);
-      const body = { url, filename: public_id };
-      const image = await ProductImg.create(body);
-      imageArray.push(image);
-  }
-
-  return res.status(201).json(imageArray);
+const create = catchError(async(req, res) => {
+  const { path, filename } = req.file;
+  const { url, public_id } = await uploadToCloudinary(path, filename);
+  const body = { url, filename: public_id }
+  const image = await ProductImg.create(body);
+  return res.status(201).json(image);
 });
 
 
-
-
-const remove = catchError(async (req, res) => {
+const remove = catchError(async(req, res) => {
   const { id } = req.params;
   const image = await ProductImg.findByPk(id);
-
-  if (!image) return res.sendStatus(404);
-
+  if(!image) return res.sendStatus(404);
   await deleteFromCloudinary(image.filename);
-
-  const localFilePath = path.join('temp', image.filename);
-  if (fs.existsSync(localFilePath)) {
-    fs.unlinkSync(localFilePath);
-  }
-
   await image.destroy();
   return res.sendStatus(204);
 });
--- src/routes/productImg.router.js
+++ src/routes/productImg.router.js
@@ -1,14 +1,13 @@
 const { getAll, create, remove } = require('../controllers/productImg.controllers');
 const express = require('express');
 const verifyJWT = require('../utils/verifyJWT');
-const multer = require("../utils/multer");
 const upload = require('../utils/multer');
 
 const routerProductImg = express.Router();
 
 routerProductImg.route('/')
     .get(verifyJWT, getAll)
-    .post(upload.array('images', 3), create);
+    .post(upload.single('image'), create);
 
 routerProductImg.route('/:id')
     .delete(verifyJWT, remove);

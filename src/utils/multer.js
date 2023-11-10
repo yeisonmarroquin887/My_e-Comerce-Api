97dbb28efcff6a978ccb15e356abc1617c143f5a
Yeison Andrés Marroquín Bernal <121591987+yeisonmarroquin887@users.noreply.github.com>
2023-11-10 16:04:51 -0500

    My Apy

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,14 +1,22 @@
-const multer = require("multer")
+const multer = require("multer");
 const path = require('path');
 
+// Configuración de Multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '..', 'public', 'uploads'));
+    },
+    filename: (req, file, cb) => {
+        // Cambié el nombre del archivo para evitar posibles conflictos
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix);
+    },
+});
+
+// Configuración del objeto de multer
 const upload = multer({
-    dest: path.join(__dirname, '..', 'public', 'uploads'),
-    storage: multer.diskStorage({
-        filename: (req, file, cb) => {
-            cb(null, file.originalname);
-        },
-        destination: path.join(__dirname, '..', 'public', 'uploads'),
-    }),
-})
+    storage: storage,
+    // Puedes añadir más configuraciones según tus necesidades
+});
 
 module.exports = upload;
